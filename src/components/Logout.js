72263eeac12043eb64ea2f950c0b33ce5644b124
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-28 02:06:42 -0500

    Logout Authenticated

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Logout = () => {        
+const Logout = () => {
+
+    const { push } = useHistory();
+    
+    useEffect(() => {
+        axiosWithAuth().post(`/logout`)
+            .then(res => {
+                localStorage.removeItem('token');
+                push('/');
+            }).catch(err => console.error(err))
+    }, [])
     return(<div></div>);
 }
 
